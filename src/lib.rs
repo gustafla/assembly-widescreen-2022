6dee09aacef91651a9f9bd0f4b64baec4dd973e9
Lauri Gustafsson <luutifa@jkry.org>
2019-04-24 23:50:12 +0300

    Fix function signatures

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
-extern "C" fn scene_init(width: i32, height: i32) -> i32 {
+use std::os::raw::c_char;
+
+#[no_mangle]
+extern "C" fn scene_init(width: i32, height: i32, get: extern "C" fn(*const c_char) -> f64) -> i32 {
     0
 }
 
+#[no_mangle]
 extern "C" fn scene_deinit() {
 }
 
+#[no_mangle]
 extern "C" fn scene_render(time: f64) {
 }
+
