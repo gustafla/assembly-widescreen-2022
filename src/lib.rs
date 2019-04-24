375e7bb73b13fdbc8a2a57038ca95e53722f3b20
Lauri Gustafsson <luutifa@jkry.org>
2019-04-25 01:46:49 +0300

    Pass scene data as raw ptrs in C-like way

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,41 @@
 use std::os::raw::c_char;
-use std::ffi::CString;
+use std::ffi::{CString, c_void};
 
 struct Scene {
-    sync_get_raw: Option<extern "C" fn(*const c_char) -> f64>,
+    sync_get_raw: extern "C" fn(*const c_char) -> f64,
+    resolution: (i32, i32),
 }
 
-static mut SCENE: Scene = Scene { sync_get_raw: None };
-
-#[no_mangle]
-extern "C" fn scene_init(width: i32, height: i32, get: extern "C" fn(*const c_char) -> f64) -> i32 {
-    unsafe {
-        SCENE.sync_get_raw = Some(get);
-    }
-    0
-}
-
-#[no_mangle]
-extern "C" fn scene_deinit() {
-}
-
-fn sync_get(name: &str) -> f64 {
-    let string = CString::new(name).unwrap();
-    unsafe {
-        SCENE.sync_get_raw.unwrap()(string.as_c_str().as_ptr())
+impl Scene {
+    fn sync_get(&self, name: &str) -> f64 {
+        let string = CString::new(name).unwrap();
+        (self.sync_get_raw)(string.as_c_str().as_ptr())
     }
 }
 
 #[no_mangle]
-extern "C" fn scene_render(time: f64) {
-    println!("Value of test is {}", sync_get("test"));
+extern "C" fn scene_init(w: i32, h: i32, get: extern "C" fn(*const c_char) -> f64) -> *mut c_void {
+    let scene = Box::new(Scene {
+        sync_get_raw: get,
+        resolution: (w, h),
+    });
+
+    Box::into_raw(scene) as *mut c_void
+}
+
+#[no_mangle]
+extern "C" fn scene_deinit(data: *mut c_void) {
+    let _scene = unsafe {Box::from_raw(data as *mut Scene)};
+}
+
+#[no_mangle]
+extern "C" fn scene_render(time: f64, data: *mut c_void) {
+    let scene = unsafe {Box::from_raw(data as *mut Scene)};
+    println!("-----------------------");
+    println!("Time is {}", time);
+    println!("Value of test is {}", scene.sync_get("test"));
+    println!("Value of width is {}", scene.resolution.0);
+    println!("Value of height is {}", scene.resolution.1);
+    std::mem::forget(scene);
 }
 
