11407ba91b3e3de63669a911f7c7d67099e25adf
Lauri Gustafsson <luutifa@jkry.org>
2019-04-25 00:46:32 +0300

    Add code to test calling demo_sync_get_value

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
 use std::os::raw::c_char;
+use std::ffi::CString;
 
 struct Scene {
-    sync_get: Option<extern "C" fn(*const c_char) -> f64>,
+    sync_get_raw: Option<extern "C" fn(*const c_char) -> f64>,
 }
 
-static mut SCENE: Scene = Scene { sync_get: None };
+static mut SCENE: Scene = Scene { sync_get_raw: None };
 
 #[no_mangle]
 extern "C" fn scene_init(width: i32, height: i32, get: extern "C" fn(*const c_char) -> f64) -> i32 {
     unsafe {
-        SCENE.sync_get = Some(get);
+        SCENE.sync_get_raw = Some(get);
     }
     0
 }
@@ -18,8 +19,15 @@ extern "C" fn scene_init(width: i32, height: i32, get: extern "C" fn(*const c_ch
 extern "C" fn scene_deinit() {
 }
 
+fn sync_get(name: &str) -> f64 {
+    let string = CString::new(name).unwrap();
+    unsafe {
+        SCENE.sync_get_raw.unwrap()(string.as_c_str().as_ptr())
+    }
+}
+
 #[no_mangle]
 extern "C" fn scene_render(time: f64) {
-    println!("Scene render called");
+    println!("Value of test is {}", sync_get("test"));
 }
 
