5254fdb1b472b17e28dd8152ee2a05ac756100f3
Lauri Gustafsson <luutifa@jkry.org>
2019-04-25 00:23:36 +0300

    Add static struct for init and render shared state

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 use std::os::raw::c_char;
 
+struct Scene {
+    sync_get: Option<extern "C" fn(*const c_char) -> f64>,
+}
+
+static mut SCENE: Scene = Scene { sync_get: None };
+
 #[no_mangle]
 extern "C" fn scene_init(width: i32, height: i32, get: extern "C" fn(*const c_char) -> f64) -> i32 {
+    unsafe {
+        SCENE.sync_get = Some(get);
+    }
     0
 }
 
@@ -11,5 +20,6 @@ extern "C" fn scene_deinit() {
 
 #[no_mangle]
 extern "C" fn scene_render(time: f64) {
+    println!("Scene render called");
 }
 
