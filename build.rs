f9f6018589bc39e055786db0f3cc1706a34733b1
Lauri Gustafsson <luutifa@jkry.org>
2019-05-08 23:38:03 +0300

    Slightly improve build script

--- build.rs
+++ build.rs
@@ -4,14 +4,17 @@ use std::env;
 
 fn main() {
     // Copy resource files from resources to target directory
+    let target_dir = Path::new(&env::var("OUT_DIR").unwrap())
+        .join("..")
+        .join("..")
+        .join("..")
+        .canonicalize().unwrap();
+
     let resource_file_paths = fs::read_dir("resources").unwrap();
+
     for dir_entry in resource_file_paths {
         let source_path = dir_entry.unwrap().path();
-        let target_path = Path::new(&env::var("OUT_DIR").unwrap())
-            .join("..")
-            .join("..")
-            .join("..")
-            .join(source_path.file_name().unwrap());
+        let target_path = target_dir.join(source_path.file_name().unwrap());
 
         println!("source_path {}", source_path.display());
         println!("target_path {}", target_path.display());
