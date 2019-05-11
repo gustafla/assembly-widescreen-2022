6e376ab0f19591e19a83ad5c4ab2ca35fb770215
Lauri Gustafsson <luutifa@jkry.org>
2019-05-12 02:07:00 +0300

    Add shader compilation and linking helpers

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
+#![allow(dead_code)]
+
+mod gles2_helpers;
+
 use std::os::raw::c_char;
 use std::ffi::{CString, c_void};
+use opengles::glesv2;
 
 struct Scene {
     sync_get_raw: extern "C" fn(*const c_char) -> f64,
@@ -15,6 +20,8 @@ impl Scene {
 
 #[no_mangle]
 extern "C" fn scene_init(w: i32, h: i32, get: extern "C" fn(*const c_char) -> f64) -> *mut c_void {
+    glesv2::viewport(0, 0, w, h);
+
     let scene = Box::new(Scene {
         sync_get_raw: get,
         resolution: (w, h),
@@ -29,11 +36,28 @@ extern "C" fn scene_deinit(data: *mut c_void) {
 }
 
 #[no_mangle]
-extern "C" fn scene_render(time: f64, data: *mut c_void) {
-    use opengles::glesv2;
+extern "C" fn scene_render(_time: f64, data: *mut c_void) {
+    let _scene = Box::leak(unsafe {Box::from_raw(data as *mut Scene)});
 
-    let scene = Box::leak(unsafe {Box::from_raw(data as *mut Scene)});
-    glesv2::clear_color(1., 0., 0., 1.);
+    let vertices = [
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.0, 0.5, 0.0
+    ];
+
+    let buffer = glesv2::gen_buffers(1)[0];
+    glesv2::bind_buffer(glesv2::GL_ARRAY_BUFFER, buffer);
+    glesv2::buffer_data(glesv2::GL_ARRAY_BUFFER, &vertices, glesv2::GL_STATIC_DRAW);
+
+    let program = gles2_helpers::link_program(&["shader.vert", "shader.frag"]).unwrap();
+    glesv2::use_program(program);
+
+    glesv2::vertex_attrib_pointer_offset(0, 3, glesv2::GL_FLOAT, false, 3 * (32/8), 0);
+    glesv2::enable_vertex_attrib_array(0);
+
+    glesv2::draw_arrays(glesv2::GL_TRIANGLES, 0, 3);
+
+    glesv2::clear_color(1., 1., 0., 1.);
     glesv2::clear(glesv2::GL_COLOR_BUFFER_BIT);
 }
 
