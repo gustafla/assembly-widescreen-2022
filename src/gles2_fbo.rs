ed415af9bd3f327ec2ba4cef79e83f4753d83b57
Lauri Gustafsson <luutifa@jkry.org>
2019-07-25 19:11:58 +0300

    Work on FBOs

--- src/gles2_fbo.rs
+++ src/gles2_fbo.rs
@@ -1,92 +1,154 @@
-use opengles::glesv2::{self, GLuint, GLenum, GLint, GLsizei};
+use opengles::glesv2::{self, GLenum, GLint, GLsizei, GLuint};
+
+enum Attachment {
+    Texture2D(GLuint),
+    Renderbuffer(GLuint),
+}
 
 #[derive(Default)]
 pub struct Fbo {
-    pub frame_buffer: GLuint,
-    pub render_buffer: Option<GLuint>,
-    pub textures: Vec<GLuint>,
+    frame_buffer: GLuint,
+    depth_attachment: Option<Attachment>,
+    stencil_attachment: Option<Attachment>,
+    color_attachment: Option<Attachment>,
 }
 
 impl Fbo {
     pub fn bind(&self) {
         glesv2::bind_framebuffer(glesv2::GL_FRAMEBUFFER, self.frame_buffer);
     }
+
+    fn attachments(&self) -> Vec<(&Option<Attachment>, GLuint)> {
+        vec![
+            (&self.depth_attachment, glesv2::GL_DEPTH_ATTACHMENT),
+            (&self.stencil_attachment, glesv2::GL_STENCIL_ATTACHMENT),
+            (&self.color_attachment, glesv2::GL_COLOR_ATTACHMENT0),
+        ]
+    }
 }
+
+const DEFAULT: Fbo = Fbo {
+    frame_buffer: 0,
+    depth_attachment: None,
+    stencil_attachment: None,
+    color_attachment: None,
+};
 
 impl Drop for Fbo {
     fn drop(&mut self) {
-        if let Some(rbo) = self.render_buffer {
-            glesv2::delete_renderbuffers(&[rbo]);
+        for attachment in self.attachments().iter()
+        {
+            match attachment {
+                (Some(Attachment::Texture2D(texture)), _) => glesv2::delete_textures(&[*texture]),
+                (Some(Attachment::Renderbuffer(rb)), _) => glesv2::delete_renderbuffers(&[*rb]),
+                _ => {}
+            }
         }
-        glesv2::delete_textures(&self.textures);
         glesv2::delete_framebuffers(&[self.frame_buffer]);
     }
 }
 
 #[derive(Debug)]
 pub enum Error {
-    RboLimit,
+    UnknownAttachment,
+    ReservedAttachment,
+    IncompleteFramebuffer,
 }
 
+#[derive(Default)]
 pub struct FboBuilder {
     fbo: Fbo,
 }
 
 impl FboBuilder {
+    pub fn new() -> Self {
+        FboBuilder::default()
+    }
+
+    fn ref_for_gl_enum(&mut self, attachment: GLenum) -> Result<&mut Option<Attachment>, Error> {
+        match attachment {
+            glesv2::GL_COLOR_ATTACHMENT0 => Ok(&mut self.fbo.color_attachment),
+            glesv2::GL_DEPTH_ATTACHMENT => Ok(&mut self.fbo.depth_attachment),
+            glesv2::GL_STENCIL_ATTACHMENT => Ok(&mut self.fbo.stencil_attachment),
+            _ => Err(Error::UnknownAttachment),
+        }
+    }
+
     pub fn add_render_buffer(
         mut self,
         format: GLenum,
         size: (GLsizei, GLsizei),
+        attachment: GLenum,
     ) -> Result<Self, Error> {
-        if let Some(_) = self.fbo.render_buffer {
-            Err(Error::RboLimit)
+        let target_att = self.ref_for_gl_enum(attachment)?;
+        if let Some(_) = target_att {
+            Err(Error::ReservedAttachment)
         } else {
             let rbo = glesv2::gen_renderbuffers(1)[0];
             glesv2::bind_renderbuffer(glesv2::GL_RENDERBUFFER, rbo);
             glesv2::renderbuffer_storage(glesv2::GL_RENDERBUFFER, format, size.0, size.1);
-            self.fbo.render_buffer = Some(rbo);
+            *target_att = Some(Attachment::Renderbuffer(rbo));
             Ok(self)
         }
     }
 
-    pub fn add_texture(mut self, format: GLenum, size: (GLsizei, GLsizei)) -> Self {
-        let tex = glesv2::gen_textures(1)[0];
-        glesv2::bind_texture(glesv2::GL_TEXTURE_2D, tex);
-        glesv2::tex_image_2d(
-            glesv2::GL_TEXTURE_2D,
-            0,
-            format as GLint,
-            size.0,
-            size.1,
-            0,
-            format,
-            glesv2::GL_UNSIGNED_BYTE,
-            &[0], // TODO probably crashes
-        );
-        self.fbo.textures.push(tex);
-        self
+    pub fn add_texture(
+        mut self,
+        format: GLenum,
+        size: (GLsizei, GLsizei),
+        attachment: GLenum,
+    ) -> Result<Self, Error> {
+        let target_att = self.ref_for_gl_enum(attachment)?;
+        if let Some(_) = target_att {
+            Err(Error::ReservedAttachment)
+        } else {
+            let tex = glesv2::gen_textures(1)[0];
+            glesv2::bind_texture(glesv2::GL_TEXTURE_2D, tex);
+            glesv2::tex_image_2d(
+                glesv2::GL_TEXTURE_2D,
+                0,
+                format as GLint,
+                size.0,
+                size.1,
+                0,
+                format,
+                glesv2::GL_UNSIGNED_BYTE,
+                &[0], // TODO probably crashes
+            );
+            *target_att = Some(Attachment::Texture2D(tex));
+            Ok(self)
+        }
     }
 
-    pub fn build(self) -> Result<Fbo, Error> {
-        let fbo = glesv2::gen_framebuffers(1)[0];
-        glesv2::bind_framebuffer(glesv2::GL_FRAMEBUFFER, fbo);
-        if let Some(rbo) = self.fbo.render_buffer {
-            glesv2::framebuffer_renderbuffer(
-                glesv2::GL_FRAMEBUFFER,
-                glesv2::GL_DEPTH_ATTACHMENT,
-                glesv2::GL_RENDERBUFFER,
-                rbo,
-            );
+    pub fn build(mut self) -> Result<Fbo, Error> {
+        self.fbo.frame_buffer = glesv2::gen_framebuffers(1)[0];
+        self.fbo.bind();
+
+        for attachment in self.fbo.attachments().iter() {
+            match attachment {
+                (Some(Attachment::Texture2D(texture)), gl_enum) => glesv2::framebuffer_texture_2d(
+                    glesv2::GL_FRAMEBUFFER,
+                    *gl_enum,
+                    glesv2::GL_TEXTURE_2D, // TODO might want to pass this
+                    *texture,
+                    0, // Mipmap level must be 0
+                ),
+                (Some(Attachment::Renderbuffer(rbo)), gl_enum) => glesv2::framebuffer_renderbuffer(
+                    glesv2::GL_FRAMEBUFFER,
+                    *gl_enum,
+                    glesv2::GL_RENDERBUFFER,
+                    *rbo,
+                ),
+                _ => {}
+            };
         }
-        for texture in &self.fbo.textures {
-            glesv2::framebuffer_texture_2d(
-                glesv2::GL_FRAMEBUFFER,
-                glesv2::GL_COLOR_ATTACHMENT0, // TODO might want to pass this
-                glesv2::GL_TEXTURE_2D, // TODO might want to pass this
-                *texture,
-                0, // Mipmap level must be 0
-            );
-        };
-        Ok(self.fbo)
+
+        if glesv2::check_framebuffer_status(glesv2::GL_FRAMEBUFFER)
+            == glesv2::GL_FRAMEBUFFER_COMPLETE
+        {
+            Ok(self.fbo)
+        } else {
+            Err(Error::IncompleteFramebuffer)
+        }
     }
 }
