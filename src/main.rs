b8dc2b0eb920d310b2c85e1abea1af35b2c00602
Lauri Gustafsson <me@gustafla.space>
2021-02-18 00:41:47 +0200

    Remove support for exclusive fullscreen

--- src/main.rs
+++ src/main.rs
@@ -6,66 +6,11 @@ use glutin::{
     dpi::PhysicalSize,
     event::{Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    platform::unix::{EventLoopWindowTargetExtUnix, WindowBuilderExtUnix},
+    platform::unix::WindowBuilderExtUnix,
     window::{Fullscreen, WindowBuilder},
     Api, ContextBuilder, GlRequest,
 };
 use std::convert::TryInto;
-
-#[derive(Debug, Default)]
-struct FullscreenOptions {
-    monitor: Option<usize>,
-    mode: usize,
-}
-
-impl FullscreenOptions {
-    fn configure<T>(self, event_loop: &EventLoop<T>) -> Result<Fullscreen> {
-        let monitor = match self.monitor {
-            Some(monitor_id) => Some(
-                event_loop
-                    .available_monitors()
-                    .nth(monitor_id)
-                    .context("Requested monitor does not exist")?,
-            ),
-            None => None,
-        };
-
-        // Use borderless fullscreen on Wayland
-        if event_loop.is_wayland() {
-            return Ok(Fullscreen::Borderless(monitor));
-        }
-
-        // Use exclusive fullscreen on other platforms
-        Ok(Fullscreen::Exclusive(
-            monitor
-                .unwrap_or_else(|| {
-                    event_loop.primary_monitor().unwrap_or_else(|| {
-                        log::info!("Can't determine primary monitor, using first");
-                        event_loop
-                            .available_monitors()
-                            .next()
-                            .expect("Can't enumerate monitors")
-                    })
-                })
-                .video_modes()
-                .nth(self.mode)
-                .context("Requested video mode does not exist")?,
-        ))
-    }
-}
-
-impl std::str::FromStr for FullscreenOptions {
-    type Err = std::num::ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut split = s.splitn(2, ',');
-
-        Ok(Self {
-            monitor: split.next().map(|s| s.parse()).transpose()?,
-            mode: split.next().map(|s| s.parse().unwrap_or(0)).unwrap_or(0),
-        })
-    }
-}
 
 fn print_monitors<T>(event_loop: EventLoop<T>) {
     for (i, monitor) in event_loop.available_monitors().enumerate() {
@@ -95,20 +40,26 @@ fn main() -> Result<()> {
 
     // Process CLI
     let mut pargs = pico_args::Arguments::from_env();
-
     if pargs.contains("--list-monitors") {
         print_monitors(event_loop);
         return Ok(());
     }
-
-    // Parse fullscreen options with fallback
-    let fullscreen: Option<FullscreenOptions> = pargs
+    let fullscreen: Option<usize> = pargs
         .opt_value_from_str("--fullscreen")
-        .unwrap_or_else(|_| Some(Default::default()));
+        .context("--fullscreen requires monitor number")?;
+    let rest = pargs.finish();
+    if !rest.is_empty() {
+        return Err(anyhow!("Unrecognized arguments {:?}", rest));
+    }
 
-    // Fullscreen options to winit configuration
+    // Configure for the specified monitor
     let fullscreen = match fullscreen {
-        Some(fullscreen_opt) => Some(fullscreen_opt.configure(&event_loop)?),
+        Some(id) => Some(
+            event_loop
+                .available_monitors()
+                .nth(id)
+                .context("Requested monitor doens't exist")?,
+        ),
         None => None,
     };
 
@@ -117,11 +68,10 @@ fn main() -> Result<()> {
         .with_title(title)
         .with_app_id("demo".into())
         .with_inner_size(match &fullscreen {
-            Some(Fullscreen::Borderless(Some(monitor))) => monitor.size(),
-            Some(Fullscreen::Exclusive(video_mode)) => video_mode.size(),
-            _ => default_size,
+            Some(monitor) => monitor.size(),
+            None => default_size,
         })
-        .with_fullscreen(fullscreen.clone())
+        .with_fullscreen(fullscreen.map(|monitor| Fullscreen::Borderless(Some(monitor))))
         .with_resizable(false)
         .with_decorations(false);
     let windowed_context = ContextBuilder::new()
@@ -131,13 +81,6 @@ fn main() -> Result<()> {
         .with_hardware_acceleration(None)
         .build_windowed(window_builder, &event_loop)
         .context("Failed to build a window")?;
-
-    // Correct borderless fullscreen inner size
-    if let Some(Fullscreen::Borderless(None)) = fullscreen {
-        if let Some(monitor) = windowed_context.window().current_monitor() {
-            windowed_context.window().set_inner_size(monitor.size());
-        }
-    }
 
     // Make OpenGL context current
     let windowed_context = unsafe { windowed_context.make_current() }
